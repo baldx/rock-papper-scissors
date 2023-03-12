fa24af5c3c6e93fd871c1cee248cf6396e81662b
baldx <mr.ahmad.m2006@gmail.com>
2023-03-12 18:40:09 +0100

    add functions and logic

---
+++ script.js
@@ -0,0 +1,14 @@
+prompt("Welcome to rock paper scissors, type anything to continue")
+console.log(playerValue)
+
+function getComputerChoice(computerChoice, randomValue) {
+      computerChoice = "rock", "paper", "scissors"
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Choose from rock paper scissors")
+    computerSelection = getComputerChoice()
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        console.log("Tie!")
+    }
+}
