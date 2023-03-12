4427cdba245fb03d58170ee686f76c1d02d2ef8e
baldx <mr.ahmad.m2006@gmail.com>
2023-03-12 18:57:55 +0100

    computerChoice work

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-prompt("Welcome to rock paper scissors, type anything to continue")
-console.log(playerValue)
+prompt("Welcome to rock paper scissors\nChoose from rock paper scissors")
 
-function getComputerChoice(computerChoice, randomValue) {
-      computerChoice = "rock", "paper", "scissors"
+function getComputerChoice(computerChoice) {
+    computerChoice = Math.floor(Math.random() * 100);
+    if (computerChoice <= 34) {
+        computerChoice = "rock";
+    }
+    else if (computerChoice <= 67 && computerChoice > 34) {
+        computerChoice = "paper";
+    }
+    else {
+        computerChoice = "scissors";
+    }
+    return computerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt("Choose from rock paper scissors")
     computerSelection = getComputerChoice()
     if (playerSelection == "rock" && computerSelection == "rock") {
         console.log("Tie!")
