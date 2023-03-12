25281c461b9db8afd9adb1d6fc8cd91c03758bb3
baldx <mr.ahmad.m2006@gmail.com>
2023-03-12 19:30:15 +0100

    fix bug with unlimited rounds

--- script.js
+++ script.js
@@ -12,38 +12,42 @@ function getComputerChoice(computerChoice) {
     return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    computerSelection = getComputerChoice()
-    playerSelection = prompt("Welcome to rock paper scissors\nChoose from rock paper scissors")
-    if (playerSelection == "rock" && computerSelection == "rock") {
-        console.log("Tie!");
+
+    function playRound(playerSelection, computerSelection ) {
+        computerSelection = getComputerChoice()
+        for (i = 0; i < 5; i++) {
+            playerSelection = prompt("Welcome to rock paper scissors\nChoose from rock paper scissors")
+            if (playerSelection == "rock" && computerSelection == "rock") {
+                console.log("Tie!");
+                }
+                else if (playerSelection == "paper" && computerSelection == "rock"){
+                    console.log("You won!");
+                }
+                else if (playerSelection == "scissors" && computerSelection == "rock") {
+                    console.log("You lost!");
+                }
+                else if (playerSelection == "rock" && computerSelection == "paper") {
+                    console.log("You lost!");
+                }
+                else if (playerSelection == "paper" && computerSelection == "paper"){
+                    console.log("Tie!");
+                }
+                else if (playerSelection == "scissors" && computerSelection == "paper") {
+                    console.log("You won!");
+                }
+                else if (playerSelection == "rock" && computerSelection == "scissors") {
+                    console.log("You won!");
+                }
+                else if (playerSelection == "paper" && computerSelection == "scissors"){
+                    console.log("You lost!");
+                }
+                else if (playerSelection == "scissors" && computerSelection == "scissors") {
+                    console.log("Tie!");
+                }
+                else {
+                    console.log("Invalid input")
+                }
+        }
+
+
     }
-    else if (playerSelection == "paper" && computerSelection == "rock"){
-        console.log("You won!");
-    }
-    else if (playerSelection == "scissors" && computerSelection == "rock") {
-        console.log("You lost!");
-    }
-    else if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log("You lost!");
-    }
-    else if (playerSelection == "paper" && computerSelection == "paper"){
-        console.log("Tie!");
-    }
-    else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log("You won!");
-    }
-    else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log("You won!");
-    }
-    else if (playerSelection == "paper" && computerSelection == "scissors"){
-        console.log("You lost!");
-    }
-    else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        console.log("Tie!");
-    }
-    else {
-        console.log("Invalid input");
-    }
-    return playRound()
-}
